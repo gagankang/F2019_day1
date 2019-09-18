5d9116e2cd01f1072d07f5a436c13b40339e9260
gagankang <preetkang4511@gmail.com>
2019-09-18 19:21:15 -0400

    [Gagan] Comment added

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -5,10 +5,15 @@ public class FirstDay
     public static  void main(String[] args)
     {
         System.out.println("Gagan");
+        // Variable Declaration
         int a, b, c;
+
+        //Value Assignment
         a = 10;
         b = 20;
         c = a + b ;
+
+        //Display Result
         System.out.println("SUM : " + c);
     }
 }
