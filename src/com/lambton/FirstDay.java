52ff13d9932aef328fcb1abad65554e23e1b10cd
gagankang <preetkang4511@gmail.com>
2019-09-18 20:37:21 -0400

    [Gagan]  Entered Student data

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -15,6 +15,16 @@ public class FirstDay
 
 
         System.out.println("SUM : " + c);
+        int x = Integer.MAX_VALUE;
+        System.out.println("X : " + x);
+
+        int y = x + 1;
+        System.out.println("Y : " + y);
+        System.out.println("Min : " + Integer.MAX_VALUE);
+
+        Student s1 = new Student();
+        s1.setvalues();
+        s1.display();
 
 
     }
--- src/com/lambton/Student.java
+++ src/com/lambton/Student.java
@@ -5,5 +5,19 @@ public class Student
     int studentID;
     String studentName;
 
-    
+    void  setvalues()
+    {
+        studentID = 1;
+        studentName = "Gagan";
+    }
+
+    void display()
+    {
+        System.out.println("Student ID : " + studentID);
+        System.out.println("Student Name : " + studentName);
+
+
+    }
+
+
 }
